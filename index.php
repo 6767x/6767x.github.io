3cf6df380dc39c27c2ec1ae8b3b9fb84fdeff643
6767x <nl601049@gmail.com>
2026-01-31 03:53:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>My Website</title>
+</head>
+<body>
 
+<?php
+// Include the Discord logger (make sure the path is correct)
+include("Discord.php");
+
+// Create an instance and run the logger
+$logger = new Discord();
+$logger->Visitor();
+?>
+
+<h1>Welcome to My Website</h1>
+<p>Your content goes here.</p>
+
+</body>
+</html>
